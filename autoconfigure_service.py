34acf12d79382090fdce8c0e2d2525f5cd8fff6a
Robert1991 <rwaddell91@googlemail.com>
2022-05-19 10:28:43 +0200

    improved logging in autoconfigure service

--- autoconfigure_service.py
+++ autoconfigure_service.py
@@ -2,10 +2,14 @@
 
 import json
 import sys
+import logging
 from ha_mqtt import connect_mqtt_client
 from ha_mqtt import auto_configure_motion_sensor
 from ha_mqtt import auto_configure_sensor
 from ha_mqtt import auto_configure_docker_update_sensor
+
+logging.basicConfig(filename='/var/log/autoconfigure_service.log',
+                    format='%(asctime)s %(message)s', level=logging.INFO)
 
 
 if len(sys.argv) != 2:
@@ -15,6 +19,7 @@ if len(sys.argv) != 2:
 
 def autoconfigure_sensors(client, config_data):
     for sensor in config_data["sensors"]:
+        logging.info("Autoconfiguring: " + sensor["name"])
         if hasattr(sensor, "device_class"):
             if sensor["device_class"] == "motion":
                 auto_configure_motion_sensor(client, config_data, sensor)
@@ -31,13 +36,15 @@ with open(sys.argv[1]) as configuration_file:
     autoconfigure_sensors(client, config_data)
 
     def on_connect(client, userdata, flags, rc):
-        print("Connected with result code "+str(rc))
+        logging.info("Connected with result code "+str(rc))
         client.subscribe("homeassistant/status")
 
     def on_message(client, userdata, msg):
+        logging.info(msg.topic+" "+str(msg.payload))
         if str(msg.payload) == "b'online'":
+            logging.info(
+                "HASS Birth message received. Autoconfiguring defined sensors...")
             autoconfigure_sensors(client, config_data)
-        print(msg.topic+" "+str(msg.payload))
 
     client.on_connect = on_connect
     client.on_message = on_message
