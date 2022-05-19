0fa418e8661db9952a162a46e06c81b17bea9d46
Robert1991 <rwaddell91@googlemail.com>
2022-05-19 10:28:07 +0200

    improved error handling in backup folder and formatting

--- backup_folder.py
+++ backup_folder.py
@@ -9,11 +9,12 @@ from os import stat
 from os import remove
 import zipfile
 import re
-import sys, getopt
+import sys
+import getopt
 import time
 
 
-def delete_files_in_folder_older_than(folder_path, days = 4):
+def delete_files_in_folder_older_than(folder_path, days=4):
     now = time.time()
     for file in listdir(folder_path):
         full_file_path = path.join(folder_path, file)
@@ -22,13 +23,19 @@ def delete_files_in_folder_older_than(folder_path, days = 4):
                 print("deleting: " + full_file_path)
                 remove(full_file_path)
 
+
 def zipdir(directory_path, ziph):
-    for root, dirs, files in walk(directory_path):
+    for root, _, files in walk(directory_path):
         for file in files:
             try:
-                ziph.write(path.join(root, file), path.relpath(path.join(root, file), path.join(directory_path, '..')))
+                ziph.write(path.join(root, file), path.relpath(
+                    path.join(root, file), path.join(directory_path, '..')))
             except:
-                print("Unexpected error:", sys.exc_info()[0])
+                caught = sys.exc_info()[0]
+                print("Unexpected error:" + str(caught) +
+                      " at file: " + str(file))
+                if caught == KeyboardInterrupt:
+                    break
 
 
 def create_backup_zip_file_name(source_folder):
@@ -37,15 +44,17 @@ def create_backup_zip_file_name(source_folder):
     dir_name = path.basename(folder_base_path)
     return dir_name + "_" + str(time_stamp) + "_backup.zip"
 
+
 def usage():
     print("usage: backup_folder.py -c <opt:cleanup files in folder older than x days> -i <input_folder> -o <output_folder>")
 
+
 try:
     if len(sys.argv) > 1:
-        opts, args = getopt.getopt(sys.argv[1:],"c:i:o:")
+        opts, args = getopt.getopt(sys.argv[1:], "c:i:o:")
     else:
         usage()
-        sys.exit(2)        
+        sys.exit(2)
 except getopt.GetoptError as err:
     usage()
     print(err)
@@ -76,7 +85,8 @@ if not path.exists(target_folder):
     print("target folder does not exist")
     exit(1)
 
-backup_file_path = path.join(target_folder, create_backup_zip_file_name(source_folder))
+backup_file_path = path.join(
+    target_folder, create_backup_zip_file_name(source_folder))
 
 print("Begin creating backup at: " + backup_file_path)
 zipf = zipfile.ZipFile(backup_file_path, 'w', zipfile.ZIP_DEFLATED)
